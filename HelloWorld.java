2f5808731442d40d826cbcf20dc1e5b3e5952d10
David Rivera Ortiz <davidps32@gmail.com>
2026-02-05 12:41:35 -0800

    Initial sharing of project

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+
+/**
+ * This is my first java app. It prints out Hello World and my name to the
+ * console 
+ * @author (David Rivera Ortiz), driveraortiz@student.sdccd.edu
+ * @version (02/05/2026)
+ */
+public class HelloWorld
+{
+    public static void main(String[] args) {
+    System.out.println("Hesllo world!");
+    System.out.println("David Rivera Ortiz"); 
+    }
+}
